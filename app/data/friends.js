1be5db115b536d4857c118a01860b6871297b798
Marc <purotx2014@gmail.com>
2019-11-05 12:44:44 -0500

    make it better

---
+++ app/data/friends.js
@@ -0,0 +1,63 @@
+var friends = [{
+    name: "Robert De Niro",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY209_CR9,0,140,209_AL_.jpg",
+    scores: [4, 2, 5, 3, 3, 5, 2, 3, 1, 1]
+}, {
+    name: "Tom Hardy",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQ3ODEyNjA4Nl5BMl5BanBnXkFtZTgwMTE4ODMyMjE@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [1, 2, 5, 5, 3, 4, 2, 3, 4, 5]
+}, {
+    name: "Daniel Day-Lewis",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjE2NDY2NDc1Ml5BMl5BanBnXkFtZTcwNjAyMjkwOQ@@._V1_UY209_CR9,0,140,209_AL_.jpg",
+    scores: [3, 3, 4, 5, 2, 1, 1, 5, 5, 4]
+}, {
+    name: "Samuel L. Jackson",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [5, 2, 5, 3, 2, 2, 1, 4, 5, 3]
+}, {
+    name: "Jack Nicholson",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_UY209_CR5,0,140,209_AL_.jpg",
+    scores: [1, 1, 3, 2, 5, 3, 3, 4, 5, 3]
+}, {
+    name: "Morgan Freeman",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [3, 3, 5, 5, 4, 4, 4, 5, 2, 1]
+}, {
+    name: "Bryan Cranston",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTA2NjEyMTY4MTVeQTJeQWpwZ15BbWU3MDQ5NDAzNDc@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [1, 2, 1, 4, 2, 3, 5, 3, 2, 4]
+}, {
+    name: "Micheal Caine",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjAwNzIwNTQ4Ml5BMl5BanBnXkFtZTYwMzE1MTUz._V1_UY209_CR5,0,140,209_AL_.jpg",
+    scores: [4, 4, 4, 3, 5, 3, 5, 5, 5, 5]
+}, {
+    name: "Tom Hanks",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY209_CR2,0,140,209_AL_.jpg",
+    scores: [1, 1, 3, 1, 2, 1, 3, 1, 1, 1]
+}, {
+    name: "Robin Williams",
+    photo: "https://m.media-amazon.com/images/M/MV5BNTYzMjc2Mjg4MF5BMl5BanBnXkFtZTcwODc1OTQwNw@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [2, 3, 2, 3, 2, 4, 3, 4, 3, 3]
+}, {
+    name: "Sam Neill",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjUyMTU0MzA0MF5BMl5BanBnXkFtZTgwNTA4MTQ4NjM@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [3, 3, 5, 1, 5, 1, 5, 2, 1, 5]
+}, {
+    name: "James Stewart",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjIwNzMzODY0NV5BMl5BanBnXkFtZTcwMDk3NDQyOA@@._V1_UY209_CR13,0,140,209_AL_.jpg",
+    scores: [2, 1, 1, 2, 1, 2, 3, 2, 2, 2]
+}, {
+    name: "Heath Ledger",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTI2NTY0NzA4MF5BMl5BanBnXkFtZTYwMjE1MDE0._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [5, 5, 5, 4, 5, 4, 5, 4, 5, 5]
+}, {
+    name: "Ian McKellen",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTQ2MjgyNjk3MV5BMl5BanBnXkFtZTcwNTA3NTY5Mg@@._V1_UY209_CR7,0,140,209_AL_.jpg",
+    scores: [2, 2, 3, 2, 5, 2, 5, 2, 5, 2]
+}, {
+    name: "Javier Bardem",
+    photo: "https://m.media-amazon.com/images/M/MV5BMTY1NTc4NTYzMF5BMl5BanBnXkFtZTcwNDIwOTY1NA@@._V1_UX140_CR0,0,140,209_AL_.jpg",
+    scores: [3, 3, 3, 3, 4, 2, 3, 2, 2, 2]
+}];
+
+module.exports = friends;
